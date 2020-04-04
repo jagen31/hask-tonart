a9067635acdd833a4a5af82175e1c0a1dc26babf
jagen31 <gentner.ja@husky.neu.edu>
2020-04-04 04:28:46 -0400

    feat: expand loops! (untested)

--- Music.hs
+++ Music.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiWayIf #-}
 
 module Music 
-  (Coord, 
-   voices,
+  (Coord(Coord), 
+   v,
    In, 
    AtomicExpr(Note, Interval),
    Expr(In, EComp, Expr), 
@@ -21,50 +22,60 @@ module Music
 import qualified Data.Set as Set
 import Data.Semigroup (sconcat)
 import Data.List (sort, find)
+import Data.Bifunctor
 import qualified Data.Map.Strict as Map
 import Control.Monad.Writer
 import Control.Exception
 import qualified Euterpea as E 
 
-type Coord = (Rational, Rational, Set.Set String)
-voices :: [String] -> Set.Set String
-voices = Set.fromList
+data Coord = 
+  Coord { start :: Rational, end :: Rational, voices :: Set.Set String } 
+  deriving (Eq, Show, Ord)
+v :: [String] -> Set.Set String
+v = Set.fromList
 
 type In = (Coord, [Expr])
-data AtomicExpr = Note E.PitchClass Int 
-                | Interval Int
+data AtomicExpr e = Note E.PitchClass Int 
+                  | Interval Int
+                  | Loop e
   deriving (Eq, Show, Ord)
 
-data Expr = In In | EComp Comp | Expr AtomicExpr deriving (Eq, Show)
+data Expr = In In | EComp Comp | Expr (AtomicExpr TopExpr) deriving (Eq, Show)
 newtype TopExpr = TopExpr [In] deriving (Eq, Show)
 
-data Comp = Comp [Coord] (Map.Map Coord [AtomicExpr]) deriving (Eq, Show)
+data Comp = Comp [Coord] (Map.Map Coord [AtomicExpr Comp]) deriving (Eq, Show, Ord)
 emptyComp :: Comp
 emptyComp = Comp [] Map.empty
 
-type ScoreVoice = [((Rational, Rational), AtomicExpr)]
+type ScoreVoice = [((Rational, Rational), AtomicExpr Comp)]
 type Score = Map.Map String ScoreVoice
 
 data MusicException = MusicException deriving (Show)
 instance Exception MusicException
 
-comp :: [(Coord, [AtomicExpr])] -> Comp
+comp :: [(Coord, [AtomicExpr Comp])] -> Comp
 comp li = Comp (foldr go [] li) $ Map.fromListWith (++) $ reverse li
   where
-    go :: (Coord, [AtomicExpr]) -> [Coord] -> [Coord]
+    go :: (Coord, [AtomicExpr Comp]) -> [Coord] -> [Coord]
     go (coord, exprs) acc = (map (\_ -> coord) exprs) ++ acc
 
 mergeCoords :: Coord -> Coord -> Coord
-mergeCoords (ps, pe, pvs) (cs, ce, cvs) = 
-  case () of _ 
-              | end > pe -> throw MusicException
-              | not (cvs `Set.isSubsetOf` pvs) -> throw MusicException
-              | otherwise -> (cs + ps, end, if (Set.null cvs) then pvs else cvs)
+mergeCoords (Coord ps pe pvs) (Coord cs ce cvs) = 
+  if | end > pe -> throw MusicException
+     | not (cvs `Set.isSubsetOf` pvs) -> throw MusicException
+     | otherwise -> Coord (cs + ps) end $ if (Set.null cvs) then pvs else cvs
   where end = ce + ps
 
 offsetComp :: Coord -> Comp -> Comp
 offsetComp pc (Comp o inner) = 
   Comp (map (mergeCoords pc) o) $ Map.mapKeys (mergeCoords pc) inner
+
+translateCoord :: Rational -> Coord -> Coord
+translateCoord offset (Coord s e v) = (Coord (s + offset) (e + offset) v)
+
+translateComp :: Rational -> Comp -> Comp
+translateComp offset (Comp o inner) = Comp (map trans o) (Map.mapKeys trans inner) 
+  where trans = (translateCoord offset)
 
 instance Semigroup Comp where 
   (<>) (Comp lOrder lMap) (Comp rOrder rMap) = 
@@ -72,11 +83,21 @@ instance Semigroup Comp where
 
 instance Monoid Comp where mempty = emptyComp
 
+compEnd :: Comp -> Rational
+compEnd (Comp o inner) = foldr max 0 $ map end (Map.keys inner)
+
+(<+>) l r = l <> translateComp (compEnd l) r
+
+convertAExpr :: AtomicExpr TopExpr -> AtomicExpr Comp
+convertAExpr (Loop e) = Loop (topExprToComp e)
+convertAExpr (Note p o) = Note p o
+convertAExpr (Interval i) = Interval i
+
 exprToComp :: Coord -> Expr -> Comp
 exprToComp pc (In (c, exprs)) = 
   mconcat (map (exprToComp (mergeCoords pc c)) exprs)
 exprToComp pc (EComp comp') = offsetComp pc comp'
-exprToComp c (Expr e) = Comp [c] $ Map.fromListWith (++) [(c, [e])]
+exprToComp c (Expr e) = Comp [c] $ Map.fromList [(c, [convertAExpr e])]
 
 topExprToComp :: TopExpr -> Comp
 topExprToComp (TopExpr ins) = 
@@ -97,8 +118,8 @@ compToScore (Comp _ comp) =
             if Map.member voice acc then 
               Map.adjust ((:) value) voice acc
             else Map.insert voice [value] acc
-    go :: Coord -> [AtomicExpr] -> Score -> Score
-    go (s, e, vs) exprs acc = 
+    go :: Coord -> [AtomicExpr Comp] -> Score -> Score
+    go (Coord s e vs) exprs acc = 
       if (length vs == 1) then insertNote (Set.elemAt 0 vs) s e exprs acc else acc
 
 -- attribution: my better half
@@ -113,7 +134,7 @@ scoreToEuterpea = foldr (\ notes m -> mkNotes notes `horz` m) (E.rest 0) where
   mkNotes notes = foldr1 (E.:+:) (reverse (snd $ foldl mkNote (0, []) notes))
 
   mkNote :: (Rational, [E.Music E.Pitch]) 
-    -> ((Rational, Rational), AtomicExpr) 
+    -> ((Rational, Rational), AtomicExpr Comp) 
     -> (Rational, [E.Music E.Pitch])
   mkNote (e, n) ((l, r), Note p o) = 
     (r, if e == l then note:n else note:(E.rest ((l - e) / 4)):n)
@@ -122,10 +143,30 @@ scoreToEuterpea = foldr (\ notes m -> mkNotes notes `horz` m) (E.rest 0) where
 
 compToEuterpea = scoreToEuterpea . compToScore
 
-main = E.play (compToEuterpea (comp [((0, 2, voices ["A"]), [Note E.D 3]), 
-                                     ((4, 6, voices ["A"]), [Note E.E 3]),
-                                     ((8, 12, voices ["A"]), [Note E.Fs 3]), 
-                                     ((0, 2, voices ["S"]), [Note E.Fs 4]), 
-                                     ((4, 6, voices ["S"]), [Note E.G 4]),
-                                     ((8, 12, voices ["S"]), [Note E.A 4])]))
+expandExpr :: Coord -> AtomicExpr Comp -> Comp
+expandExpr c (Loop comp) = 
+  mconcat $ take (floor (((end c) - (start c)) / len)) $ loop comp
+  where len = compEnd comp
+        loop comp = comp : loop (translateComp len comp)
+expandExpr _ _ = emptyComp
+
+expandComp :: Comp -> Comp
+expandComp (Comp [] inner) = (Comp [] inner)
+expandComp (Comp (o:os) inner) = 
+  case (Map.lookup o inner) of Just (e:es) -> (go (expandedRest es) e) <> expandExpr o e
+  where expandedRest es = expandComp $ (Comp os $ Map.insert o es inner)
+        go (Comp restO restInner) e = (Comp (o:restO) $ Map.adjust ((:) e) o restInner)
+
+test = (comp [(Coord 0 1 $ v ["A"], [Note E.D 3]), 
+              (Coord 2 3 $ v ["A"], [Note E.E 3]),
+              (Coord 4 6 $ v ["A"], [Note E.Fs 3]), 
+              (Coord 0 1 $ v ["S"], [Note E.Fs 4]), 
+              (Coord 2 3 $ v ["S"], [Note E.G 4]),
+              (Coord 4 6 $ v ["S"], [Note E.A 4])])
+
+bass = (comp [(Coord 0 (1/2) $ v ["B"], [Note E.D 2]),
+              (Coord (1/2) 1 $ v ["B"], [Note E.A 2])])
+
+main = E.play (compToEuterpea $ expandComp 
+  ((test <+> test) <> (comp [(Coord 0 12 $ v ["B"], [Loop bass])])))
 
--- Spec.hs
+++ Spec.hs
@@ -7,66 +7,69 @@ import Control.Exception (evaluate)
 main = hspec $ do
   describe "Music.mergeCoords" $ do
     it "merges coordinates" $
-      mergeCoords (2, 4, voices ["S", "A"]) (1, 2, voices ["S"]) `shouldBe` (3, 4, voices ["S"])
+      mergeCoords (Coord 2 4 $ v ["S", "A"])
+                  (Coord 1 2 $ v ["S"]) `shouldBe` (Coord 3 4 $ v ["S"])
 
     it "errors when range is beyond end of parent" $
-      evaluate (mergeCoords (2, 4, voices ["S", "A"]) (1, 3, voices ["S"])) `shouldThrow` anyException
+      evaluate (mergeCoords (Coord 2 4 $ v ["S", "A"]) 
+                            (Coord 1 3 $ v ["S"])) `shouldThrow` anyException
 
-    it "errors when voices are not a subset of parent" $
-      evaluate (mergeCoords (2, 4, voices ["S", "A"]) (1, 2, voices ["T"]))
+    it "errors when $ v are not a subset of parent" $
+      evaluate (mergeCoords (Coord 2 4 $ v ["S", "A"]) (Coord 1 2 $ v ["T"]))
         `shouldThrow` anyException
 
-    it "inherits parent when voices are empty" $
-      mergeCoords (2, 4, voices ["S", "A"]) (1, 2, voices []) `shouldBe` (3, 4, voices ["S", "A"])
+    it "inherits parent when $ v are empty" $
+      mergeCoords (Coord 2 4 $ v ["S", "A"]) 
+                  (Coord 1 2 $ v []) `shouldBe` (Coord 3 4 $ v ["S", "A"])
 
   describe "Music.topExprToComp" $ do
     it "translates an empty expression" $
       topExprToComp (TopExpr []) `shouldBe` emptyComp
    
     it "translates an expression" $
-      let example = TopExpr [((0, 4, voices ["S"]), [Expr $ Note E.A 4])] in
+      let example = TopExpr [((Coord 0 4 $ v ["S"]), [Expr $ Note E.A 4])] in
         topExprToComp example `shouldBe`
-          comp [((0, 4, voices ["S"]), [Note E.A 4])]
+          comp [((Coord 0 4 $ v ["S"]), [Note E.A 4])]
     
     it "translates a nested expression" $
       let example =
-            TopExpr [((4, 8, voices ["S", "A"]), 
+            TopExpr [((Coord 4 8 $ v ["S", "A"]), 
                      [Expr $ Interval 4,
-                      In ((0, 2, voices ["S"]), [Expr $ Note E.Fs 4]),
-                      In ((2, 4, voices ["A"]), [Expr $ Note E.D 4])])] in
+                      In ((Coord 0 2 $ v ["S"]), [Expr $ Note E.Fs 4]),
+                      In ((Coord 2 4 $ v ["A"]), [Expr $ Note E.D 4])])] in
         topExprToComp example `shouldBe`
-          comp [((4, 8, voices ["S", "A"]), [Interval 4]), 
-                ((4, 6, voices ["S"]), [Note E.Fs 4]), 
-                ((6, 8, voices ["A"]), [Note E.D 4])]
+          comp [((Coord 4 8 $ v ["S", "A"]), [Interval 4]), 
+                ((Coord 4 6 $ v ["S"]), [Note E.Fs 4]), 
+                ((Coord 6 8 $ v ["A"]), [Note E.D 4])]
 
     it "gets ordering right" $
       let example =
-            TopExpr [((4, 6, voices ["S"]), [Expr $ Note E.C 4]),
-                     ((4, 8, voices ["S", "A"]), 
+            TopExpr [((Coord 4 6 $ v ["S"]), [Expr $ Note E.C 4]),
+                     ((Coord 4 8 $ v ["S", "A"]), 
                      [Expr $ Interval 4,
-                      In ((0, 2, voices ["S"]), [Expr $ Note E.Fs 4]),
-                      In ((2, 4, voices ["A"]), [Expr $ Note E.D 4])])] in
+                      In ((Coord 0 2 $ v ["S"]), [Expr $ Note E.Fs 4]),
+                      In ((Coord 2 4 $ v ["A"]), [Expr $ Note E.D 4])])] in
         topExprToComp example `shouldBe`
-          comp [((4, 6, voices ["S"]), [Note E.C 4]), 
-                ((4, 8, voices ["S", "A"]), [Interval 4]), 
-                ((4, 6, voices ["S"]), [Note E.Fs 4]), 
-                ((6, 8, voices ["A"]), [Note E.D 4])]
+          comp [((Coord 4 6 $ v ["S"]), [Note E.C 4]), 
+                ((Coord 4 8 $ v ["S", "A"]), [Interval 4]), 
+                ((Coord 4 6 $ v ["S"]), [Note E.Fs 4]), 
+                ((Coord 6 8 $ v ["A"]), [Note E.D 4])]
 
     it "handles precompiled expressions" $
       let example =
-            TopExpr [((4, 6, voices ["S"]), [Expr $ Note E.C 4]),
-                     ((4, 8, voices ["S", "A"]), 
+            TopExpr [((Coord 4 6 $ v ["S"]), [Expr $ Note E.C 4]),
+                     ((Coord 4 8 $ v ["S", "A"]), 
                       [Expr $ Interval 4,
-                       In ((0, 2, voices ["S"]), [Expr $ Note E.Fs 4]),
-                       In ((2, 4, voices ["A"]), 
-                           [EComp $ comp [((0, 1, voices []), [Note E.D 3]), 
-                                          ((1, 2, voices []), [Note E.E 3])]])])] in
+                       In ((Coord 0 2 $ v ["S"]), [Expr $ Note E.Fs 4]),
+                       In ((Coord 2 4 $ v ["A"]), 
+                           [EComp $ comp [((Coord 0 1 $ v []), [Note E.D 3]), 
+                                          ((Coord 1 2 $ v []), [Note E.E 3])]])])] in
         topExprToComp example `shouldBe`
-          comp [((4, 6, voices ["S"]), [Note E.C 4]), 
-                ((4, 8, voices ["S", "A"]), [Interval 4]), 
-                ((4, 6, voices ["S"]), [Note E.Fs 4]), 
-                ((6, 7, voices ["A"]), [Note E.D 3]),
-                ((7, 8, voices ["A"]), [Note E.E 3])]
+          comp [((Coord 4 6 $ v ["S"]), [Note E.C 4]), 
+                ((Coord 4 8 $ v ["S", "A"]), [Interval 4]), 
+                ((Coord 4 6 $ v ["S"]), [Note E.Fs 4]), 
+                ((Coord 6 7 $ v ["A"]), [Note E.D 3]),
+                ((Coord 7 8 $ v ["A"]), [Note E.E 3])]
       
 
   describe "Music.compToEuterpea" $ do
@@ -75,27 +78,27 @@ main = hspec $ do
       compToEuterpea (comp []) == E.rest 0 
 
     it "converts a single line" $
-      compToEuterpea (comp [((0, 2, voices ["A"]), [Note E.D 3]), 
-                            ((2, 4, voices ["A"]), [Note E.E 3]),
-                            ((4, 8, voices ["A"]), [Note E.Fs 3])])
+      compToEuterpea (comp [((Coord 0 2 $ v ["A"]), [Note E.D 3]), 
+                            ((Coord 2 4 $ v ["A"]), [Note E.E 3]),
+                            ((Coord 4 8 $ v ["A"]), [Note E.Fs 3])])
       `shouldBe`
       E.note E.hn (E.D, 3) E.:+: E.note E.hn (E.E, 3) E.:+: E.note E.wn (E.Fs, 3)
 
     it "inserts rests" $
-      compToEuterpea (comp [((0, 2, voices ["A"]), [Note E.D 3]), 
-                            ((4, 6, voices ["A"]), [Note E.E 3]),
-                            ((8, 12, voices ["A"]), [Note E.Fs 3])])
+      compToEuterpea (comp [((Coord 0 2 $ v ["A"]), [Note E.D 3]), 
+                            ((Coord 4 6 $ v ["A"]), [Note E.E 3]),
+                            ((Coord 8 12 $ v ["A"]), [Note E.Fs 3])])
       `shouldBe`
       E.note E.hn (E.D, 3) E.:+: E.rest E.hn E.:+: 
       E.note E.hn (E.E, 3) E.:+: E.rest E.hn E.:+: 
       E.note E.wn (E.Fs, 3)
-    it "handles multiple voices " $
-      compToEuterpea (comp [((0, 2, voices ["A"]), [Note E.D 3]), 
-                            ((4, 6, voices ["A"]), [Note E.E 3]),
-                            ((8, 12, voices ["A"]), [Note E.Fs 3]), 
-                            ((0, 2, voices ["S"]), [Note E.D 4]), 
-                            ((4, 6, voices ["S"]), [Note E.E 4]),
-                            ((8, 12, voices ["S"]), [Note E.Fs 4])])
+    it "handles multiple $ v " $
+      compToEuterpea (comp [((Coord 0 2 $ v ["A"]), [Note E.D 3]), 
+                            ((Coord 4 6 $ v ["A"]), [Note E.E 3]),
+                            ((Coord 8 12 $ v ["A"]), [Note E.Fs 3]), 
+                            ((Coord 0 2 $ v ["S"]), [Note E.D 4]), 
+                            ((Coord 4 6 $ v ["S"]), [Note E.E 4]),
+                            ((Coord 8 12 $ v ["S"]), [Note E.Fs 4])])
       `shouldBe`
       (E.note E.hn (E.D, 3) E.:+: E.rest E.hn E.:+: 
        E.note E.hn (E.E, 3) E.:+: E.rest E.hn E.:+: 
